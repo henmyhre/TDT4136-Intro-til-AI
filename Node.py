36b8762e054bd08990fe698f0509de298df01d89
Henrik Øberg Myhre <henrik.myhre99@gmail.com>
2020-09-18 13:41:37 +0200

    Added comments

--- Node.py
+++ Node.py
@@ -1,25 +1,21 @@
 class Node:
+    """
+  Override eq and gt to be able to compare two nodes.
+  h, g and f are estimated cost to end node, cost from start node and the total cost respectively.
+  """
+
     def __init__(self, lowest_cost_parent=None, pos=None):
         self.pos = pos
         self.children = []
         self.lowest_cost_parent = lowest_cost_parent
         self.g = 0
         self.h = 0
-        self.f = 0
 
     def __eq__(self, other):
         return self.pos == other.pos
 
     def __gt__(self, other):
-        self.set_f()
         return self.get_f() > other.get_f()
 
-    def __str__(self):
-        self.set_f()
-        return "Position: " + str(self.pos) + ". Total cost: " + str(self.get_f())
-
-    def set_f(self):
-        self.f = self.g + self.h
-
     def get_f(self):
-        return self.f
+        return self.g + self.h
--- aStar.py
+++ aStar.py
@@ -1,10 +1,12 @@
 import Map
 from Node import Node
 
-"""Based on A* pseudo-code from Medium'"""
-
 
 def a_star_search(map, start, end):
+    """
+    Based on A* pseudo-code from Medium:
+    https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+    """
     start_node = Node(None, start)
     start_node.h = abs(start_node.pos[0] - end[0]) + \
         abs(start_node.pos[1] - end[1])
@@ -13,18 +15,21 @@ def a_star_search(map, start, end):
     open_nodes.append(start_node)
     end_node = Node(None, end)
 
+    """Code stops if no open nodes left"""
     while open_nodes:
         current_node = open_nodes.pop(0)
         closed_nodes.append(current_node)
         children = []
 
+        """Backtracking to calculate path when end node is reached"""
         if current_node == end_node:
             path = []
             while (current_node):
                 path.append(current_node.pos)
                 current_node = current_node.lowest_cost_parent
-            return path[::-1]
+            return path
 
+        """Checks all possible directions (up, right, down, left) and add them as children"""
         for direction_to_connected_node in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             node_pos = (
                 current_node.pos[0] + direction_to_connected_node[0], current_node.pos[1] + direction_to_connected_node[1])
@@ -32,16 +37,26 @@ def a_star_search(map, start, end):
                 new_node = Node(current_node, node_pos)
                 children.append(new_node)
 
+        """
+    Goes through children nodes.
+    Checks if they aldready exists in open_nodes or closed_nodes to not use recreated nodes.
+    Adds parent relation if node is 'new'.
+    Replaces parent relation and iproves children cost if new parent is better than old.
+    """
         for child in children:
             tile_cost = map[child.pos[0]][child.pos[1]]
+            open_node = False
+            closed_node = False
             for i in range(len(open_nodes)):
                 if open_nodes[i] == child:
                     child = open_nodes[i]
+                    open_node = True
             for i in range(len(closed_nodes)):
                 if closed_nodes[i] == child:
                     child = closed_nodes[i]
+                    closed_node = True
             current_node.children.append(child)
-            if child not in closed_nodes and child not in open_nodes:
+            if not open_node and not closed_node:
                 create_parent_relation(
                     child, current_node, end, tile_cost)
                 open_nodes.append(child)
@@ -50,30 +65,39 @@ def a_star_search(map, start, end):
                 create_parent_relation(
                     child, current_node, end, tile_cost)
                 if child in closed_nodes:
-                    propagate_path_improvements(child)
+                    improve_children_cost(child)
+
     return False
 
 
 def create_parent_relation(child, parent, end, tile_cost):
+    """Creates parent relation and updates child g and h cost"""
     child.lowest_cost_parent = parent
     child.g = parent.g + tile_cost
     child.h = abs(child.pos[0] - end[0]) + abs(child.pos[1] - end[1])
 
 
-def propagate_path_improvements(node):
+def improve_children_cost(node):
+    """Improves a nodes children cost if new path from given node is better"""
     for child in node.children:
         if child.g > node.g + 1:
             child.lowest_cost_parent = node
             child.g = node.g + 1
-            propagate_path_improvements(child)
+            improve_children_cost(child)
 
 
 def add_path_to_map(map, path):
+    """Adds path to map"""
     for n in path:
         map.set_cell_value(n, " p ")
 
 
 def main():
+    """
+    Runs until user stops.
+    User choses which task to show.
+    Prints both map with and without path.
+    """
     while (True):
         n = -1
         while not (5 > n > 0):
@@ -84,9 +108,13 @@ def main():
         start_pos = (map.get_start_pos()[0], map.get_start_pos()[1])
         end_pos = (map.get_goal_pos()[0], map.get_goal_pos()[1])
         path = a_star_search(int_map, start_pos, end_pos)
-        add_path_to_map(map, path)
-        int_map, str_map = map.get_maps()
-        map.show_map()
+        if (not path):
+            print('no path')
+            break
+        else:
+            add_path_to_map(map, path)
+            int_map, str_map = map.get_maps()
+            map.show_map()
         if ('q' == input("'q' to quit: ")):
             break
 
